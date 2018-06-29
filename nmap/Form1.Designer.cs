b1804256b2c1b218f5852e4a202796b53966c87d
c8246579 <cassenzzz@gmail.com>
2018-06-29 16:10:51 +0800

    initial commit

---
+++ nmap/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace nmap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cer_bt = new System.Windows.Forms.Button();
+            this.ip_box = new System.Windows.Forms.TextBox();
+            this.ip_txt = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.port_box = new System.Windows.Forms.TextBox();
+            this.port_txt = new System.Windows.Forms.Label();
+            this.ssl_bt = new System.Windows.Forms.Button();
+            this.state = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cer_bt
+            // 
+            this.cer_bt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cer_bt.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cer_bt.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.cer_bt.Location = new System.Drawing.Point(299, 15);
+            this.cer_bt.Name = "cer_bt";
+            this.cer_bt.Size = new System.Drawing.Size(97, 62);
+            this.cer_bt.TabIndex = 0;
+            this.cer_bt.Text = "Cer";
+            this.cer_bt.UseVisualStyleBackColor = true;
+            this.cer_bt.Click += new System.EventHandler(this.BtCer_Click);
+            // 
+            // ip_box
+            // 
+            this.ip_box.BackColor = System.Drawing.Color.Ivory;
+            this.ip_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ip_box.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ip_box.Location = new System.Drawing.Point(87, 15);
+            this.ip_box.Name = "ip_box";
+            this.ip_box.Size = new System.Drawing.Size(190, 26);
+            this.ip_box.TabIndex = 1;
+            // 
+            // ip_txt
+            // 
+            this.ip_txt.AutoSize = true;
+            this.ip_txt.Location = new System.Drawing.Point(10, 22);
+            this.ip_txt.Name = "ip_txt";
+            this.ip_txt.Size = new System.Drawing.Size(61, 12);
+            this.ip_txt.TabIndex = 2;
+            this.ip_txt.Text = "IP / Domain";
+            this.ip_txt.Click += new System.EventHandler(this.LbIP_Click);
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.Linen;
+            this.result.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.result.Location = new System.Drawing.Point(12, 91);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.result.Size = new System.Drawing.Size(629, 304);
+            this.result.TabIndex = 3;
+            this.result.TextChanged += new System.EventHandler(this.result_TextChanged);
+            // 
+            // port_box
+            // 
+            this.port_box.BackColor = System.Drawing.Color.Ivory;
+            this.port_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.port_box.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.port_box.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.port_box.Location = new System.Drawing.Point(87, 51);
+            this.port_box.MaxLength = 5;
+            this.port_box.Name = "port_box";
+            this.port_box.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.port_box.Size = new System.Drawing.Size(190, 26);
+            this.port_box.TabIndex = 4;
+            this.port_box.TextChanged += new System.EventHandler(this.port_box_TextChanged);
+            // 
+            // port_txt
+            // 
+            this.port_txt.AutoSize = true;
+            this.port_txt.Location = new System.Drawing.Point(12, 58);
+            this.port_txt.Name = "port_txt";
+            this.port_txt.Size = new System.Drawing.Size(24, 12);
+            this.port_txt.TabIndex = 5;
+            this.port_txt.Text = "Port";
+            this.port_txt.Click += new System.EventHandler(this.port_txt_Click);
+            // 
+            // ssl_bt
+            // 
+            this.ssl_bt.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ssl_bt.Location = new System.Drawing.Point(412, 15);
+            this.ssl_bt.Name = "ssl_bt";
+            this.ssl_bt.Size = new System.Drawing.Size(97, 62);
+            this.ssl_bt.TabIndex = 6;
+            this.ssl_bt.Text = "SSL";
+            this.ssl_bt.UseVisualStyleBackColor = true;
+            this.ssl_bt.Click += new System.EventHandler(this.BtSSL_Click);
+            // 
+            // state
+            // 
+            this.state.BackColor = System.Drawing.Color.LightGreen;
+            this.state.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.state.Location = new System.Drawing.Point(542, 14);
+            this.state.Name = "state";
+            this.state.ReadOnly = true;
+            this.state.Size = new System.Drawing.Size(72, 27);
+            this.state.TabIndex = 7;
+            this.state.Text = "等待中";
+            this.state.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.state.TextChanged += new System.EventHandler(this.state_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(654, 412);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.ssl_bt);
+            this.Controls.Add(this.port_txt);
+            this.Controls.Add(this.port_box);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.ip_txt);
+            this.Controls.Add(this.ip_box);
+            this.Controls.Add(this.cer_bt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "NMAP-scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cer_bt;
+        private System.Windows.Forms.TextBox ip_box;
+        private System.Windows.Forms.Label ip_txt;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TextBox port_box;
+        private System.Windows.Forms.Label port_txt;
+        private System.Windows.Forms.Button ssl_bt;
+        private System.Windows.Forms.TextBox state;
+    }
+}
+
---
+++ nmap/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+namespace nmap
+{
+    public partial class Form1 : Form
+    {
+        int mode = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.result.Invoke((MethodInvoker)delegate
+            {
+                if (e.Data == null)
+                {
+                    cer_bt.Enabled = true;
+                    ssl_bt.Enabled = true;
+                    state.Text = "已結束";
+                    state.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    //result.AppendText(e.Data + "\r\n");
+                    //Cer
+                    if(mode == 1)
+                    {
+                        if (e.Data.StartsWith("| Issuer:")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("| Public Key type:")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("| Public Key bits:")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("| Signature Algorithm:")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("| Not valid before:")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("| Not valid after:")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("|_SHA-1:")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("|_ssl-date:")) { result.AppendText(e.Data + "\r\n"); }
+                    }
+                    
+
+                    //SSL
+                    if(mode == 2)
+                    {
+                        if (e.Data.StartsWith("| ")) { result.AppendText(e.Data + "\r\n"); }
+                        if (e.Data.StartsWith("|_")) { result.AppendText(e.Data + "\r\n"); }
+                    }
+                    
+
+                    //common
+                    if (e.Data.StartsWith("Nmap done:")) { result.AppendText(e.Data + "\r\n"); }
+                }
+
+                
+                
+            });
+        }
+
+
+        private void BtCer_Click(object sender, EventArgs e)
+        {
+            result.Focus();
+            mode = 1;
+            if (check())
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                state.Text = "掃描中";
+                state.BackColor = Color.Coral;
+                cer_bt.Enabled = false;
+                ssl_bt.Enabled = false;
+                result.Text = "";
+                string str = Console.ReadLine();
+                
+                process.StartInfo.FileName = "nmap.bat";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                string str1 = "-sC";
+                string str2 = "-p";
+                string str3 = "-v";
+
+                if (port_box.Text == null || port_box.Text == "")
+                    port_box.Text = "443";
+
+                process.StartInfo.Arguments = string.Format(str3 + " " + str1 + " " + ip_box.Text + " " + str2 + " " + port_box.Text);
+                process.Start();
+                process.OutputDataReceived += proc_OutputDataReceived;
+                process.BeginOutputReadLine();
+                process.StandardInput.AutoFlush = true;
+                process.Close();
+                
+            }
+        }
+
+        
+
+
+        private void BtSSL_Click(object sender, EventArgs e)
+        {
+            result.Focus();
+            mode = 2;
+            if (check())
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                state.Text = "掃描中";
+                state.BackColor = Color.Coral;
+                cer_bt.Enabled = false;
+                ssl_bt.Enabled = false;
+                result.Text = "";
+                string str = Console.ReadLine();
+                
+                process.StartInfo.FileName = "nmap.bat";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                string str1 = "--script";  //--script ssl-enum-ciphers
+                string str2 = "-p";
+                string str3 = "ssl-enum-ciphers";
+
+                if (port_box.Text == null || port_box.Text == "")
+                    port_box.Text = "443";
+
+                process.StartInfo.Arguments = string.Format(str1 + " " + str3 + " " + ip_box.Text + " " + str2 + " " + port_box.Text);
+                process.Start();
+                process.OutputDataReceived += proc_OutputDataReceived;
+                process.BeginOutputReadLine();
+                process.StandardInput.AutoFlush = true;
+                process.Close();
+            }
+            
+        }
+
+        private bool check()
+        {
+            
+            Regex ipRgx = new Regex(@"^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$");
+            Regex portRgx = new Regex(@"^\d{1,5}$");
+            Regex domainRex = new Regex(@"^(([\w][\w\-\.]*)\.)?([\w][\w\-]+)(\.([\w][\w\.]*))?$");
+            if (!(portRgx.IsMatch(port_box.Text, 0)|port_box.Text==""))
+            {
+                result.Text = "port is not courrect";
+                return false;
+            }
+            if (ipRgx.IsMatch(ip_box.Text, 0)|domainRex.IsMatch(ip_box.Text,0))
+            {
+                return true;
+            }
+            else
+            {
+                result.Text = "ip is not courrect";
+                return false;
+            }
+
+        }
+        private void LbIP_Click(object sender, EventArgs e)
+        {}
+        private void result_TextChanged(object sender, EventArgs e)
+        {}
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+        private void port_box_TextChanged(object sender, EventArgs e)
+        {}
+        private void port_txt_Click(object sender, EventArgs e)
+        {}
+
+        private void state_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
